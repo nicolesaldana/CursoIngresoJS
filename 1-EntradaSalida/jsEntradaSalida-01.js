34bea1da8ef69eefa857a9dc5fe96c0c47f5c44d
unknown <nicolesaldana>
2022-02-08 21:29:08 -0300

    nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,6 +1,8 @@
-/*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+/* Nicole Saldaña
+Ejercicio E/S 1 division z
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("<3");
+	alert("ok");
+}
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,15 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
-
+	nombreIngresado=document.getElementById('txtIdNombre').value;
+    //nombreIngresado = txtIdNombre.value;
+    alert(nombreIngresado);
+	
 }
+
+//txtIdNombre
+
 
 
